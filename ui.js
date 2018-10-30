c0142060b57ea2a8c8bd1b5dea6d874f14d34fbb
Wade Stevens <wayde22@gmail.com>
2018-10-30 14:57:57 -0500

    Added flag and degree symbols

--- ui.js
+++ ui.js
@@ -8,17 +8,19 @@ class UI {
         this.min = document.getElementById('w-feel-like');
         this.wind = document.getElementById('w-dewpoint');
         this.max = document.getElementById('w-wind');
+        this.flag = document.getElementById('w-flag');
     }
 
     paint(info) {
         this.location.textContent = `${info.data.name}, ${info.data.sys.country}`;
         this.desc.textContent = info.data.weather[0].description;
         this.string.textContent = `${info.data.main.temp}° F`;
+        this.flag.setAttribute('src', `http://openweathermap.org/images/flags/${info.data.sys.country.toLowerCase()}.png`);
         this.icon.setAttribute('src', `http://openweathermap.org/img/w/${info.data.weather[0].icon}.png`);
-        this.details.textContent = `Relative Humidity: ${info.data.main.humidity}`;
-        this.min.textContent = `Min Temp: ${info.data.main.temp_min}`;
-        this.max.textContent = `Max Temp: ${info.data.main.temp_max}`;
-        this.wind.textContent = `Wind Speed: ${info.data.wind.speed}`;
+        this.details.textContent = `Relative Humidity: ${info.data.main.humidity}%`;
+        this.min.textContent = `Min Temp: ${info.data.main.temp_min}° F`;
+        this.max.textContent = `Max Temp: ${info.data.main.temp_max}° F`;
+        this.wind.textContent = `Wind Speed: ${info.data.wind.speed} MPH`;
         
     }
 }
